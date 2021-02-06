d3e580d88c6269aff3ac6894933da03cb10f68ae
divyang <divyangmistry7777@gmail.com>
2021-02-06 16:20:46 +0530

    changed the video to iframe and linked youtube dynamic video url

--- src/components/screenMyPage.js
+++ src/components/screenMyPage.js
@@ -1,34 +1,62 @@
-import React, { useState } from 'react';
-import { Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, ScrollView, Image, TouchableOpacity, useWindowDimensions } from 'react-native';
 import { withFocusable } from '@noriginmedia/react-spatial-navigation';
 import { themeStyles, hasWebFocusableUI } from '../config';
 
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
-// import Video from 'react-native-video';
+import moment from 'moment';
+import HTML from "react-native-render-html";
 
 
 const ScreenMyPage = (props) => {
 
     const [showCards, setShowCards] = useState(true);
+    const [embedURL, setEmbedURL] = useState('');
+    const [contentWidth, setContentWidth] = useState(useWindowDimensions().width);
+
+    useEffect(() => {
+        // callApi()
+        console.log(embedURL);
+    }, [embedURL]);
+
+    const callApi = async () => {
+        await fetch(`https://dbapi.dadabhagwan.org/api/tv/curtvprogram?curtime=${moment().format('kk:mm:ss')}&curdate=${moment().format('DD/MM/YYYY kk:mm:ss')}&channel=58&daytransition=0&liveflag=0`)
+            .then((response) => response.json())
+            .then(async (res) => {
+                // await setEmbedURL(res.result["curtvprogram"][0].YOUTUBE_EMBED_CODE)
+                await setEmbedURL(res.result["curtvprogram"][0].YOUTUBE_EMBED_CODE.split('src="')[1].split('" ')[0])
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
     return <View style={themeStyles.screen}>
         <ScrollView contentContainerStyle={themeStyles.container}>
-            {/* https://www.dadabhagwan.tv/library/pujya+deepakbhai+sange+satsang+thi+soneri+prabhat+part-1000/ */}
-            {showCards ? <><TouchableOpacity onPress={() => setShowCards(false)}>
+            {showCards ? <><TouchableOpacity onPress={async () => {
+                await callApi()
+                setShowCards(false)
+            }}>
                 <Card>
                     <Card.Title style={{ width: 400 }}>Watch in Gujarati</Card.Title>
                     <Card.Divider />
                     <Card.Image source={{ uri: 'https://satsang.dadabhagwan.org/assets/img/video.svg' }} />
                 </Card>
             </TouchableOpacity>
-                <TouchableOpacity onPress={() => setShowCards(false)}>
+                <TouchableOpacity onPress={async () => {
+                    await callApi()
+                    setShowCards(false)
+                }}>
                     <Card>
                         <Card.Title style={{ width: 400 }}>Watch in Hindi</Card.Title>
                         <Card.Divider />
                         <Card.Image source={{ uri: 'https://satsang.dadabhagwan.org/assets/img/video.svg' }} />
                     </Card>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => setShowCards(false)}>
+                <TouchableOpacity onPress={async () => {
+                    await callApi()
+                    setShowCards(false)
+                }}>
                     <Card>
                         <Card.Title style={{ width: 400 }}>Watch in English</Card.Title>
                         <Card.Divider />
@@ -37,17 +65,23 @@ const ScreenMyPage = (props) => {
                 </TouchableOpacity>
             </> :
                 <>
-                    <video id="video" src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
-                        width='100%' height="auto" poster="https://satsang.dadabhagwan.org/assets/img/video.svg"
-                        preload="auto" autoPlay={true}  controls loop>
-                    </video>
-                    {/* <Video source={{ uri: 'https://youtu.be/5W4yzUeL8cE?t=385' }}   // Can be a URL or a local file.
-                        ref={(ref) => {
-                            console.log(ref);
-                        }}                                      // Store reference
-                        onBuffer={() => { console.log('inside buffer !!!'); }}                // Callback when remote video is buffering
-                        onError={() => { console.log('inside Error !!!'); }}               // Callback when video cannot be loaded
-                        style={styles.backgroundVideo} /> */}
+                    <iframe src={embedURL} frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        style={{
+                            position: 'fixed', top: 0, left: 0, bottom: 0,
+                            right: 0, width: '100%', height: '100%',
+                            border: 'none', margin: 0, padding: 0, overflow: 'hidden', zIndex: 999
+                        }}>
+                    </iframe>
+
+                    {/* <HTML source={{ html: embedURL }} contentWidth={contentWidth} /> */}
+                    {/* {embedURL && embedURL.length > 0 &&
+                        <video id="video" width='100%' height="auto"
+                            poster="https://satsang.dadabhagwan.org/assets/img/video.svg"
+                            preload="auto" autoPlay={true} controls loop>
+                            <source src={embedURL} type="video/youtube" />
+                        </video>} */}
                 </>
             }
         </ScrollView>
